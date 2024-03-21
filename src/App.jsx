ccea868f838debd6e3664fc4e4c07a95df9b4e12
Iqyan Aziz Syamaidzar <azizsyam503@gmail.com>
2024-03-21 10:56:19 +0700

    finish-setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex justify-center">
+      <div className="text-5xl text-red-700">Hello World!</div>
+    </div>
+  );
+}
+
+export default App;
