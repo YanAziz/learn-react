66c265172dfc942fdb054c9d1cfa21a62dfab258
Iqyan Aziz Syamaidzar <azizsyam503@gmail.com>
2024-03-21 13:56:55 +0700

    component-props

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,46 @@
 import "./App.css";
+import PropTypes from "prop-types";
+
+const Button = (props) => {
+  const { children = "Register", variant = "bg-blue-700" } = props;
+  return (
+    <button className={`h-10 px-6 font-semibold rounded-md text-white ${variant}`} type="submit">
+      {children}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  variant: PropTypes.string, // Define the 'variant' prop as a string
+  children: PropTypes.string,
+};
+
+function ButtonBlack() {
+  return (
+    <button className="h-10 px-6 font-semibold rounded-md bg-black text-white" type="submit">
+      Buy now
+    </button>
+  );
+}
+
+const ButtonRed = () => {
+  return (
+    <button className="h-10 px-6 font-semibold rounded-md bg-red-700 text-white" type="submit">
+      Buy now
+    </button>
+  );
+};
 
 function App() {
   return (
-    <div className="flex justify-center">
-      <div className="text-5xl text-red-700">Hello World!</div>
+    <div className="flex justify-center min-h-screen bg-blue-300 items-center">
+      <div className="flex gap-x-3">
+        <Button variant="bg-red-700">Login</Button>
+        <Button variant="bg-slate-700">Logout</Button>
+        <Button></Button>
+        <ButtonRed />
+        <ButtonBlack />
+      </div>
     </div>
   );
 }
